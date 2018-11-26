80c466ff8469ae2d712744367ebcfb80731dbfcf
Peter Imoesi <peterimoesi@yahoo.com>
2018-11-26 23:52:51 +0200

    fixed meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import routes from './routes';
+
 import Navigation from './screens/navbar';
 import Notification from './components/notification';
 
@@ -11,6 +13,41 @@ class App extends React.Component {
             <div className="App" id="App">
                 <Navigation {...this.props} />
                 <Notification />
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <meta content="Entergrate" name="title" />
+                    <meta property="og:title" content="Entergrate Finland" />
+                    <meta
+                        name="description"
+                        content="Entergrate is a social enterprise for immigrants in Finland."
+                    />
+                    <meta
+                        property="og:description"
+                        content="Entergrate is a social enterprise for immigrants in Finland."
+                    />
+                    <meta
+                        property="og:image"
+                        content="%PUBLIC_URL%/img/about/2.png"
+                    />
+                    {/* Chrome, Firefox OS and Opera */}
+                    <meta name="theme-color" content="#212529" />
+                    {/* <!-- Windows Phone --> */}
+                    <meta
+                        name="msapplication-navbutton-color"
+                        content="#212529"
+                    />
+                    {/* <!-- iOS Safari --> */}
+                    <meta
+                        name="apple-mobile-web-app-status-bar-style"
+                        content="#212529"
+                    />
+
+                    <title>Entergrate - Homepage</title>
+                </Helmet>
                 <div id="app-routes">{routes}</div>
             </div>
         );
